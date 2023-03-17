2b20defc1a4c6634d98381ee7582435425b3e799
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-18 00:12:24 +0100

    tasks1&6

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i, j, m, f1, f2, l1, l2;
 
-	for (i = 0; i < 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 1; j < 99; j++)
+		for (j = 1; j < 100; j++)
 		{
 			f1 = i / 10;
 			l1 = i % 10;
