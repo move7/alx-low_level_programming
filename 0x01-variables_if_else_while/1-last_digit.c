4094b8ed571c2136a0d13edc249648c8278b23bf
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-17 11:33:10 +0100

    tasks1&2

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - main function return if last number is > 5 or < 6 or == 0
+*Return: 0 success
+*/
+int main(void)
+{
+	int n, rest;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	rest = n % 10;
+	if (rest > 5)
+			printf("Last digit of %d is %d and is greater than 5\n", n, rest);
+	else
+		if (rest == 0)
+			printf("Last digit of %d is %d and is 0\n", n, rest);
+		else
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rest);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - main function
+*Return: 0 success
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
