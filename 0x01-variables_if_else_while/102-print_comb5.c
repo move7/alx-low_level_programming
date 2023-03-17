00da8e8596b9a28091aac64317c0b7aa4443c7b1
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-18 00:05:28 +0100

    tasks1&6

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - main function
+*Return: 0 success
+*/
+int main(void)
+{
+	int i, j, m, f1, f2, l1, l2;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = 1; j < 99; j++)
+		{
+			f1 = i / 10;
+			l1 = i % 10;
+			f2 = j / 10;
+			l2 = j % 10;
+			putchar(f1 + 48);
+			putchar(l1 + 48);
+			putchar(' ');
+			putchar(f2 + 48);
+			putchar(l2 + 48);
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
