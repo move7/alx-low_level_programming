0a90cd733fa879c8a96a88feae0039bbe0ab2a72
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-17 13:27:29 +0100

    tasks1&6

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - main function
+*Return: 0 success
+*/
+int main(void)
+{
+	int c, n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (c = 48; c <= 57; c++)
+		{
+			putchar(n);
+			putchar(c);
+			if (c != 57 || n != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
