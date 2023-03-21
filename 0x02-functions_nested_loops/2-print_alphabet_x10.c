e9eb80e12733132cd2bc4a1384a6cdb82f0955cf
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-21 20:37:20 +0100

    print_alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet - prints 10 times the alphabet
+ * print_alphabetprint_alphabet_x10 - prints 10 times the alphabet
  */
 void print_alphabet_x10(void)
 {
 int i;
+char letter;
+
 for (i = 0; i <= 10; i++)
-print_alphabet();
-_putchar('\n');
+{
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
 }
+}
+
