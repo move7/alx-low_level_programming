5648e089b5342da6b23c09c2ea5f4bb121db83c0
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-22 00:37:11 +0100

    print qlphqbet 10 times

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - check if char is alpha
+ *@c: is the char to be checked
+ *Return: 1 if char is alpha, otherwise 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
