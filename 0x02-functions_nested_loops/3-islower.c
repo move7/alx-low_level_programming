999e1abbca6208882219665536c6329811a05367
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-22 00:29:30 +0100

    print qlphqbet 10 times

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _islower - print all alphabet in lowercase
+ * _islower - check if char is lowercase
  *Return: 1 if char is lowercase, otherwise 0
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
