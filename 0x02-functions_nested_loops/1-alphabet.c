81de963bdff39f184340f6921627b6402bd5f4fc
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-21 20:03:06 +0100

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*main - prints the alphabet, in lowercase, followed by a new line.
+*Return: 0 success
+*/
+void print_alphabet(void)
+{
+  int i;
+for(i=97; i<=122; i++){
+  _putchar(i);
+}
+_putchar('\n');
+return (0);
+}
