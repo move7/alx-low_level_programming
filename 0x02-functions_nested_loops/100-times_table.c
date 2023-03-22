c5e6b479b787ba403a3642f56559f18369feb911
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-22 02:50:59 +0100

    print qlphqbet 10 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,26 +5,32 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k;
-
-	for (i = 0; i <= n; i++)
+	if (n < 15 && n > 0)
 	{
-		for (j = 0; j <= n; j++)
+		int i, j, k;
+
+		for (i = 0; i <= n; i++)
 		{
-			k = i * j;
-			if (k >= 10)
-				_putchar((k / 10) + '0');
-			else if (j > 0)
-				_putchar(' ');
-			_putchar((k % 10) + '0');
-			if (j == n)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
+				k = i * j;
+				if (k >= 10)
+					_putchar((k / 10) + '0');
+				else if (j > 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar((k % 10) + '0');
+				if (j == n)
+				{
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 	}
