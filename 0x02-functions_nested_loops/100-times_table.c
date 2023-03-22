04817ae0073905f900069a752238980f3c05e469
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-22 02:44:45 +0100

    print qlphqbet 10 times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			if (k >= 10)
+				_putchar((k / 10) + '0');
+			else if (j > 0)
+				_putchar(' ');
+			_putchar((k % 10) + '0');
+			if (j == n)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
