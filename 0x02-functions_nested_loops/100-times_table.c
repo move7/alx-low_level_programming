d62c4c14bad989fbc7df5f183ad9683487e64b71
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-22 03:06:20 +0100

    print qlphqbet 10 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 					k = k % 100;
 					_putchar((k / 10) + '0');
 				}
-				else if (k >= 100)
+				else if (k >= 10)
 				{
 					_putchar(' ');
 					_putchar((k / 10) + '0');
