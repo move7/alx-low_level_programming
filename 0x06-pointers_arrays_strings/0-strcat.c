4fabba8a2426814406a7e1c3348f467678297c74
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-31 01:01:05 +0200

    all tasks

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *_strcat - concatenates two strings
+  *@dest: input value
+  *@src: input value
+  *Return: dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
