08ee0c7ca942eef65d8278ec419c4a347295090b
root <root@Ubuntu.myguest.virtualbox.org>
2023-04-04 02:07:00 +0200

    all tasks from 0 to 7

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,14 +4,13 @@
  * @s: address of memory to be filled
  * @b: constant byte
  * @n: number of bytes to be replaced
- *
  * Return: changed array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for (i=0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
