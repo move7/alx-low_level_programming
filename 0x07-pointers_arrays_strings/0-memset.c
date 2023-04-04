c180cfe50778e46bdacc688535022e54a61f5471
root <root@Ubuntu.myguest.virtualbox.org>
2023-04-04 02:01:13 +0200

    all tasks from 0 to 7

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: address of memory to be filled
+ * @b: constant byte
+ * @n: number of bytes to be replaced
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (i=0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
