071a7696d3327e9d7f4351bc17951bbd7d1b58b6
root <root@Ubuntu.myguest.virtualbox.org>
2023-04-06 00:51:57 +0200

    all tasks from 0 to 7

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,23 @@
 #include "main.h"
 
-int actual_sqrt_recursion(int n, int i);
+/**
+ * find_sqrt_recursion - find the natural square root of a number
+ * @n: number to calculate the square root of
+ * @i: iterator
+ *
+ * Return: square root
+ */
+int find_sqrt_recursion (int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt_recursion - returns natural square root of a numbe
  * @n: input
  *
  * Return: square root
@@ -12,5 +26,6 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (find_sqrt_recursion(n, 0));
 }
+
