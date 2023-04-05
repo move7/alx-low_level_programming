8fea286cae39316cb7ba4f96daa220c630424b00
root <root@Ubuntu.myguest.virtualbox.org>
2023-04-06 01:01:27 +0200

    all tasks from 0 to 7

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * checker - check if a number is a prime 
+ * @n: number to check
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, otherwise 0
+ */
+int checker(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i)
+		return (0);
+	return (checker (n, i - 1));
+}
+
+/**
+ * is_prime_number - if the input integer is a prime number
+ * @n: input
+ *
+ * Return: 1 if n is a prime number, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
