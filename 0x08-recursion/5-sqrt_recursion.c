f6d99ff6ee355406e121f8cdb757b9146c2a68af
root <root@Ubuntu.myguest.virtualbox.org>
2023-04-06 00:53:22 +0200

    all tasks from 0 to 7

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,7 +13,7 @@ int find_sqrt_recursion (int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	return (find_sqrt_recursion (n, i + 1));
 }
 
 /**
