5672509046d72e1d1571603f93714172a72aac2e
root <root@Ubuntu.myguest.virtualbox.org>
2023-04-12 00:15:29 +0200

    tasks calloc, malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr – concatenates all the arguments
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, r = 0, length = 0;
+	char *result;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j]; j++)
+			length++;
+	length + = ac;
+	result = malloc(sizeof(char) * length + 1);
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (j = 0; av[i][j]; j++)
+	{
+		str[r] = av[i][j];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	}
+	return (result);
+}
+
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,52 @@
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+
+char **strtow(char *str)
+{
+	char **result, *tmp;
+	int i, k = 0, length = 0, words = 0, c = 0, start, end;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			space = 1;
+		else if (space == 1)
+		{ words++;
+			space = 0;
+		}
+		length++;
+	}
+	if (words == 0)
+		return (NULL);
+	result = (char **) malloc(sizeof(char *) * (words + 1));
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i <= length; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < i)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+	matrix[k] = NULL;
+	return (matrix);
+}
+
