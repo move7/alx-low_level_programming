953f11cbfe958e3e9b968e779c979679912576cd
root <root@Ubuntu.myguest.virtualbox.org>
2023-04-14 03:42:35 +0200

    all tasks

---
+++ 0x0C-more_malloc_free/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *concat;
+
+    concat = string_nconcat("Best ", "School !!!", 16);
+    printf("%s\n", concat);
+    printf("%ld\n", sizeof(concat)); 
+    free(concat);
+    return (0);
+}
