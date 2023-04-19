8c948396fa1abcca9b2fb514fa4722360d67d6e8
root <root@Ubuntu.myguest.virtualbox.org>
2023-04-20 00:31:39 +0200

    all tasks

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - prints each array elem
+ * on a new line
+ * @array: array
+ * @size: size of array
+ * @action: pointer to print function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
+
