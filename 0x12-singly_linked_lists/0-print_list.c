3ba02e587a933c8e810b969aba7e1b2d07bdf074
fatima <addouni.fatima@gmail.com>
2023-04-28 02:33:38 +0200

    all tasks

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * _strlen - length of a given string
+ * @str: String
+ *
+ * Return: the length of the str
+ */
+int _strlen(char *str)
+{
+        int length = 0;
+
+        if (!str)
+                return (0);
+        while (*str++)
+                length++;
+        return (length);
+}
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to be printed
+ *
+ * Return: the number of printed node
+ */
+size_t print_list(const list_t *h)
+{
+        size_t sum = 0;
+
+        while (h)
+        {
+                if (!h->str)
+                        printf("[0] (nil)\n");
+                else
+                        printf("[%u] %s\n", _strlen(h->str), h->str);
+                h = h->next;
+                sum++;
+        }
+        return (sum);
+}
