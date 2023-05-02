66950824eb02d473593e379c47ed4b90191c4bf9
fatima <addouni.fatima@gmail.com>
2023-05-03 01:01:12 +0200

    task 9

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a list
+ * @head: pointer to the first node
+ *
+ * Return: pointer to the first node of the new list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+ listint_t *prev = NULL;
+ listint_t *next = NULL;
+
+ while (*head)
+ {
+ next = (*head)->next;
+ (*head)->next = prev;
+ prev = *head;
+ *head = next;
+ }
+
+ *head = prev;
+
+ return (*head);
+}
+
+ node = h->next;
+ h->next = node->next;
+ free(node);
+
+ return (1);
+}
+
