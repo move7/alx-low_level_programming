163ee775b2854b8099170a0ad9dc6373e3156427
move7 <addouni.fatima@gmail.com>
2023-06-16 01:11:09 +0200

    tasks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	idx--;
-	for (idx--)
+	while (idx--)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
