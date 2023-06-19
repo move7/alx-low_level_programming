c5fab7850a26514b2bd967bdfa5aea5381d382ab
move7 <addouni.fatima@gmail.com>
2023-06-19 16:02:07 -0700

    Code injection: Win the Giga Millions

--- 0x18-dynamic_libraries/rand.c
+++ 0x18-dynamic_libraries/rand.c
@@ -3,10 +3,10 @@
 
 int rand()
 {
-	static int count = 0;
-	int win_numbers[] ={9, 8, 10, 24, 75, 9};
-	count++;
-	if(count <= 5)
-		return(win_numbers[count]);
-	return(count*count/1000);
+        static int count = 0;
+        int win_numbers[] ={9, 8, 10, 23, 74, 9};
+        count++;
+        if(count <= 5)
+                return(win_numbers[count]);
+        return(count*count/1000);
 }
