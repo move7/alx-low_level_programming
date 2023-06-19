0a98eec74d0955b9c5f6700c02c6d6714e3fc203
move7 <addouni.fatima@gmail.com>
2023-06-19 16:11:19 -0700

    task3

--- 0x18-dynamic_libraries/rand.c
+++ 0x18-dynamic_libraries/rand.c
@@ -4,7 +4,7 @@
 int rand()
 {
         static int count = 0;
-        int win_numbers[] ={9, 8, 10, 23, 74, 9};
+        int win_numbers[] ={8, 8, 7, 9, 23, 74};
         count++;
         if(count <= 5)
                 return(win_numbers[count]);
