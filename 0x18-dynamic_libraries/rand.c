4c37348484d92ec27aa7d456dfbb917355b686bb
move7 <addouni.fatima@gmail.com>
2023-06-19 13:13:58 -0700

    task3

---
+++ 0x18-dynamic_libraries/rand.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <string.h>
+
+int rand()
+{
+	static int count = 0;
+	int win_numbers[] ={9, 8, 10, 24, 75, 9};
+	count++;
+	if(count <= 5)
+		return(win_numbers[count]);
+	return(count*count/1000);
+}
