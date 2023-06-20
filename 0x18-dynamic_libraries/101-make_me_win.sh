1d48a3089dbbeee572670633c02a5ff8332768f5
move7 <addouni.fatima@gmail.com>
2023-06-20 14:33:13 -0700

    last try

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/move7/alx-low_level_programming/blob/master/0x18-dynamic_libraries/rand.so
+wget -P /tmp https://github.com/move7/alx-low_level_programming/raw/master/0x18-dynamic_libraries/rand.so
 export LD_PRELOAD=/tmp/rand.so
